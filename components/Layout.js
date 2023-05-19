7403da82357907e6ebfd3ac150d7c3a523920cad
henritresor <shimwamanahenritresor@gmail.com>
2023-05-19 08:00:45 +0200

    changed file names

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Header from './Header'
-const Layout = ({children}) => {
+import Header from './Header/Index'
+const Layout = ({ children }) => {
   return (
-      <>
-          <Header />
-          {children}
-      </>
+    <>
+      <Header />
+      {children}
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Header from '../components/Header'
-import GameCard from '../components/GameCard'
+import Header from '../components/Header/Index'
+import GameCard from '../components/GameCard/Index'
 import { useEffect } from 'react'
 import { Box, Typography, Grid, Card, Button } from '@mui/material'
 import axios from 'axios'
 import Carousel from 'react-material-ui-carousel'
 import Link from 'next/link'
 import Meta from '../components/Meta'
-import SimpleCard from '../components/Card'
+import SimpleCard from '../components/Card/Index'
 import { AddBoxOutlined } from '@mui/icons-material'
 
 export const getStaticProps = async () => {
@@ -26,30 +26,30 @@ const Index = ({ TopGames, otherGames }) => {
   // console.log(TopGames);
   return (
     <>
-      <Meta title={'Pixel Portal - Home' } />
+      <Meta title={'Pixel Portal - Home'} />
       <Box
         sx={{ marginTop: '2em' }}
       >
         <Typography
           variant='h6'
-          sx={{ml:5, mb:1}}
+          sx={{ ml: 5, mb: 1 }}
         >
           Trending Games
         </Typography>
         <Carousel
           duration={700}
           fullHeightHover={false}
-         IndicatorIcon=''
+          IndicatorIcon=''
         >
-            {
-              TopGames?.map(game => {
-                return (
-                  <Link href={`/games/${game.slug}`} passHref key={game?.id}>
+          {
+            TopGames?.map(game => {
+              return (
+                <Link href={`/games/${game.slug}`} passHref key={game?.id}>
                   <GameCard  {...game} />
-               </Link>
-                )
-              })
-            }
+                </Link>
+              )
+            })
+          }
         </Carousel>
       </Box>
       <Box
@@ -59,34 +59,34 @@ const Index = ({ TopGames, otherGames }) => {
           Available Categories
         </Typography>
 
-        
-            </Box>
-      <Box sx={{ marginTop: '2em', p:5 }}> 
-        <Box sx={{ display: 'flex', p: 5, mb: 1, alignItems:'center', justifyContent:'space-between'}}>
+
+      </Box>
+      <Box sx={{ marginTop: '2em', p: 5 }}>
+        <Box sx={{ display: 'flex', p: 5, mb: 1, alignItems: 'center', justifyContent: 'space-between' }}>
           <Typography variant='h6'
-            sx={{ }}>All Games</Typography>
-      <Button sx={{color:'white', background:'grey'}}>See more</Button>
-       </Box>
-        <Box 
-         sx={{display:'flex', flexWrap:'wrap'}}
+            sx={{}}>All Games</Typography>
+          <Button sx={{ color: 'white', background: 'grey' }}>See more</Button>
+        </Box>
+        <Box
+          sx={{ display: 'flex', flexWrap: 'wrap' }}
         >
           {
             otherGames?.map(game => {
               return (
-               
-                <SimpleCard  {...game} key={ game.id} />
-                  
+
+                <SimpleCard  {...game} key={game.id} />
+
               )
             })
           }
         </Box>
 
-        <Box sx={{display:'flex', justifyContent:'center', p:4}}>
-          
+        <Box sx={{ display: 'flex', justifyContent: 'center', p: 4 }}>
+
           <Button
             sx={{ color: 'white', background: 'grey' }}
           >View More</Button>
-       </Box>
+        </Box>
       </Box>
     </>
 
