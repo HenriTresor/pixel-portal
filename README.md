b95a419b8358b93a3e007ad0bb665953a208a4e6
henritresor <shimwamanahenritresor@gmail.com>
2023-09-27 08:10:57 +0200

    re-initialised project

--- README.md
+++
@@ -1,5 +0,0 @@
-## Pixel Portal
-
-### Online Gaming App 
-
-### Made With ReactJS, NextJS and MUI
--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['media.rawg.io']
-    }
-}
-
-module.exports = nextConfig
