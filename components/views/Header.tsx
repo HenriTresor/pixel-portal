251103fe1a0cdcfb435c0a2d096f02b7e815014a
henritresor <shimwamanahenritresor@gmail.com>
2023-10-03 08:52:17 +0200

    added header responsiveness

--- components/views/Header.tsx
+++ components/views/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { navListItems } from '@/lib/constants'
 import Link from 'next/link'
 import { Button } from '../ui/button'
+import { Menu } from 'lucide-react'
 
 type Props = {}
 
@@ -10,7 +11,7 @@ function Header({ }: Props) {
         <div className='p-4 mt-5 sticky top-0 left-0 borderb-2 border-orange-600 flex items-center justify-between w-full pl-11 pr-11 bg-black'>
             <h1 className='text-[1.7rem] font-bold'>pixel <span className='text-orange-600'>portal</span></h1>
 
-            <div className='flex gap-5 items-center'>
+            <div className='hidden gap-5 items-center sm:flex'>
                 {
                     navListItems.map(item => (
                         <Link
@@ -21,6 +22,9 @@ function Header({ }: Props) {
                 }
                 <Button>Contact us</Button>
             </div>
+            <Button size={'icon'} className='flex sm:hidden'>
+                <Menu />
+            </Button>
         </div>
     )
 }
